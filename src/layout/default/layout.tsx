1fcb542d3011ce57475afb0433b4a176def79189
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-04-18 14:13:14 +0200

    feat: implement footer

--- src/layout/default/layout.tsx
+++ src/layout/default/layout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 import styles from './layout.module.scss';
 import Container from '../../packages/components/container/container';
 import Header from '../../packages/components/header/header';
+import Footer from '../../packages/components/footer/footer';
 
 export default function Layout() {
   return (
@@ -12,6 +13,7 @@ export default function Layout() {
           <Outlet />
         </Container>
       </main>
+      <Footer />
     </div>
   );
 }
