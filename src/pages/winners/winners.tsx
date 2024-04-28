547dc0a7eb3e4d9123591f7f4604131f9c836e7c
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-04-29 01:10:37 +0200

    feat: show winners count

--- src/pages/winners/winners.tsx
+++ src/pages/winners/winners.tsx
@@ -1,14 +1,22 @@
 import { FC } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store/store';
 import { IPageProps } from '../types';
 import Page from '../page';
 import WinnersList from './winners-list/winners-list';
 import WinnersPagination from './winners-pagination/winners-pagination';
+import pageStyles from '../page.module.scss';
 
-const WinnersPage: FC<Pick<IPageProps, 'visible'>> = ({ visible }: { visible: boolean }) => (
-  <Page visible={visible}>
-    <WinnersList />
-    <WinnersPagination />
-  </Page>
-);
+const WinnersPage: FC<Pick<IPageProps, 'visible'>> = ({ visible }: { visible: boolean }) => {
+  const { totalCount } = useSelector((state: RootState) => state.winners);
+
+  return (
+    <Page visible={visible}>
+      <h2 className={pageStyles.title}>Winners: {totalCount}</h2>
+      <WinnersList />
+      <WinnersPagination />
+    </Page>
+  );
+};
 
 export default WinnersPage;
