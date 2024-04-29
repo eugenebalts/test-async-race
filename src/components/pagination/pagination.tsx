1e87a825ebaabc4877bbde8c77eaaed571d14252
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-04-29 16:09:45 +0200

    feat: add color to pagination props

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -3,9 +3,9 @@ import { Pagination } from '@mui/material';
 import { IPaginationProps } from './types';
 import styles from './pagination.module.scss';
 
-const CustomPagination: FC<IPaginationProps> = ({ count, page, onChange }) => (
+const CustomPagination: FC<IPaginationProps> = ({ color = 'primary', count, page, onChange }) => (
   <div className={styles.wrapper}>
-    <Pagination count={count} page={page} color='primary' onChange={onChange} />
+    <Pagination color={color} count={count} page={page} onChange={onChange} />
   </div>
 );
 
--- src/components/pagination/types.ts
+++ src/components/pagination/types.ts
@@ -3,5 +3,6 @@ import { ChangeEvent } from 'react';
 export interface IPaginationProps {
   count: number;
   page: number;
+  color?: 'primary' | 'secondary',
   onChange: (event: ChangeEvent<unknown>, value: number) => void;
 }
