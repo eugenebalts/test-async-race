a7b109ad41fa7535ed8a5a12ca5bb8439ac4b569
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-04-29 23:44:47 +0200

    feat: implement alert component (snackbar)

---
+++ src/components/snackbar/types.ts
@@ -0,0 +1,10 @@
+import { SnackbarOrigin } from '@mui/material';
+
+export interface ISnackbarState extends SnackbarOrigin {
+  isOpen: boolean;
+}
+
+export interface ISnackbarProps {
+  cause: boolean;
+  message: string; 
+}
