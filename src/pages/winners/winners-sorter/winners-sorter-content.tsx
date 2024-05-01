497e1fff0dac6aa9d14ec35e97c2a00a4a4bf7d9
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-05-01 23:14:29 +0200

    feat: use vars in sorter, set checked on state depending

--- src/pages/winners/winners-sorter/winners-sorter-content.tsx
+++ src/pages/winners/winners-sorter/winners-sorter-content.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, FC } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { FormControlLabel, Radio, RadioGroup } from '@mui/material';
-import { AppDispatch } from '../../../redux/store/store';
+import { AppDispatch, RootState } from '../../../redux/store/store';
 import { IWinnersSortOptions } from '../../../redux/store/slices/winners/types';
 import { winnersActions } from '../../../redux/store/slices/winners';
 import { IWinnersSorterProps } from './types';
@@ -11,6 +11,13 @@ const generateSortOptionValue = ({ sort, order }: IWinnersSortOptions) =>
   JSON.stringify({ sort, order });
 
 const WinnersSorter: FC<IWinnersSorterProps> = ({ onChange }) => {
+  const ID_ASC: IWinnersSortOptions = { sort: 'id', order: 'ASC' };
+  const WINS_ASC: IWinnersSortOptions = { sort: 'wins', order: 'ASC' };
+  const WINS_DESC: IWinnersSortOptions = { sort: 'wins', order: 'DESC' };
+  const TIME_ASC: IWinnersSortOptions = { sort: 'time', order: 'ASC' };
+  const TIME_DESC: IWinnersSortOptions = { sort: 'time', order: 'DESC' };
+
+  const { sort, order } = useSelector((state: RootState) => state.winners.sortOptions);
   const dispatch = useDispatch<AppDispatch>();
   const { sortWinners } = winnersActions;
 
@@ -32,29 +39,34 @@ const WinnersSorter: FC<IWinnersSorterProps> = ({ onChange }) => {
       onChange={handleChange}
     >
       <FormControlLabel
-        value={generateSortOptionValue({ sort: 'id', order: 'ASC' })}
+        value={generateSortOptionValue(ID_ASC)}
         control={<Radio />}
         label='None'
+        checked={sort === ID_ASC.sort && order === ID_ASC.order}
       />
       <FormControlLabel
-        value={generateSortOptionValue({ sort: 'wins', order: 'ASC' })}
+        value={generateSortOptionValue(WINS_ASC)}
         control={<Radio />}
         label='Wins: Low to high'
+        checked={sort === WINS_ASC.sort && order === WINS_ASC.order}
       />
       <FormControlLabel
-        value={generateSortOptionValue({ sort: 'wins', order: 'DESC' })}
+        value={generateSortOptionValue(WINS_DESC)}
         control={<Radio />}
         label='Wins: High to low'
+        checked={sort === WINS_DESC.sort && order === WINS_DESC.order}
       />
       <FormControlLabel
-        value={generateSortOptionValue({ sort: 'time', order: 'ASC' })}
+        value={generateSortOptionValue(TIME_ASC)}
         control={<Radio />}
         label='Time: Low to high'
+        checked={sort === TIME_ASC.sort && order === TIME_ASC.order}
       />
       <FormControlLabel
-        value={generateSortOptionValue({ sort: 'time', order: 'DESC' })}
+        value={generateSortOptionValue(TIME_DESC)}
         control={<Radio />}
         label='Time: High to low'
+        checked={sort === TIME_DESC.sort && order === TIME_DESC.order}
       />
     </RadioGroup>
   );
